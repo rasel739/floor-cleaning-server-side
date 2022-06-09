1371f86a18a8334f137ef588a4adec79870e1fbd
rasel739 <raselhossaintro@gmail.com>
2022-06-09 11:39:26 +0600

    add new data

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 app.use(cors());
 
+app.use(express.static("public"));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tw0ab.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
